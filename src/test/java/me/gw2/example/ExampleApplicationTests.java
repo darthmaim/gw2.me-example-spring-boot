1419364ad2eb5233e911d3e39e4f6b684e123dbc
darthmaim <git@darthmaim.de>
2025-05-06 18:39:55 +0200

    init

---
+++ src/test/java/me/gw2/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gw2.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
