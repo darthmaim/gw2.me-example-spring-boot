e3ab71c20116999b80b302220bc47957ef1747c5
darthmaim <git@darthmaim.de>
2025-05-13 23:50:02 +0200

    Fetch GW2 accounts and generate subtokens

--- src/main/java/me/gw2/example/web/HomeController.java
+++ src/main/java/me/gw2/example/web/HomeController.java
@@ -1,19 +1,65 @@
 package me.gw2.example.web;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
+import java.io.IOException;
+
+import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
 
 @RestController
 class HomeController {
+    @Autowired
+    private RestClient restClient;
 
     @GetMapping("/")
     public ModelAndView home(@AuthenticationPrincipal @Nullable OAuth2User user) {
-        return new ModelAndView("home", user != null ? Map.of("attributes", user.getAttributes()) : null);
+        if(user == null) {
+            return new ModelAndView("home");
+        }
+
+        // create model
+        var model = new java.util.HashMap<String, Object>();
+
+        // add user attributes to model
+        model.put("attributes", user.getAttributes());
+
+        // get accounts
+        var accounts = this.restClient.get()
+                        .uri("https://gw2.me/api/accounts")
+                        .attributes(clientRegistrationId("gw2me"))
+                        .retrieve()
+                        .body(Gw2MeAccounts.class);
+
+        model.put("accounts", accounts.accounts);
+
+        return new ModelAndView("home", model);
     }
+
+    @GetMapping("/account")
+    public void account(@RequestParam String id, HttpServletResponse response) throws IOException {
+        // get subtoken
+        var subtokenResponse = this.restClient.get()
+                .uri("https://gw2.me/api/accounts/%s/subtoken".formatted(id))
+                .attributes(clientRegistrationId("gw2me"))
+                .retrieve()
+                .body(Gw2MeSubtokenResponse.class);
+
+        // redirect to official GW2 API
+        response.sendRedirect(
+                "https://api.guildwars2.com/v2/account?access_token=%s".formatted(subtokenResponse.subtoken));
+    }
+
+    record Gw2MeAccounts(Gw2MeAccount[] accounts) {}
+    record Gw2MeAccount(String id, String name, Boolean shared) {}
+
+    record Gw2MeSubtokenResponse(String subtoken) {}
 }
