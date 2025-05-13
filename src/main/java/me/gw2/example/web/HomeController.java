3d0257f5449589e57c2bcda4cae408ce8c69cfc5
darthmaim <git@darthmaim.de>
2025-05-13 22:12:56 +0200

    Remove forced auth for home page and add PKCE + `include_granted_scopes`

--- src/main/java/me/gw2/example/web/HomeController.java
+++ src/main/java/me/gw2/example/web/HomeController.java
@@ -1,18 +1,19 @@
 package me.gw2.example.web;
 
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
+import java.util.Map;
 
 @RestController
 class HomeController {
 
     @GetMapping("/")
-    public ModelAndView home(@AuthenticationPrincipal OAuth2User user) {
-        return new ModelAndView("home", Collections.singletonMap("attributes", user.getAttributes()));
+    public ModelAndView home(@AuthenticationPrincipal @Nullable OAuth2User user) {
+        return new ModelAndView("home", user != null ? Map.of("attributes", user.getAttributes()) : null);
     }
 }
